b30ccf29546fcc31725cb523407d30ea0587fda5
mauro julian acosta <acostamaurojulian1@gmail.com>
2024-06-18 21:58:03 -0300

    dev2

--- v2/gui/inicio.py
+++ v2/gui/inicio.py
@@ -1,17 +1,19 @@
 import customtkinter as ctk
 
-class InicioUi(ctk.CTk):
-    def __init__(self):
-        super().__init__()
-        self.title("Inicio")
-        self.geometry("600x400")
-        self.resizable(False, False)
-        # Aquí puedes añadir los widgets y la lógica de la pantalla de inicio
-        label = ctk.CTkLabel(self, text="Pantalla de Inicio")
-        label.pack(pady=20)
 
-        close_button = ctk.CTkButton(self, text="Cerrar", command=self.close)
-        close_button.pack(pady=20)
+class InicioGui(ctk.CTkFrame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
+        self.controller.title("Sha 256")
+        label = ctk.CTkLabel(self, text="Página de Inicio")
+        label.grid(row=0, column=0, padx=20, pady=20)
 
-    def close(self):
-        self.destroy()
+        cifrar_button = ctk.CTkButton(self, text="Ir a Cifrar", command=lambda: controller.mostrarPagina("Cifrar","Cifrar sha 256"))
+        cifrar_button.grid(row=1, column=0, padx=20, pady=10)
+        
+        btnDesifrar = ctk.CTkButton(self, text="Ir a Desifrar", command=lambda: controller.mostrarPagina("Desifrar"))
+        btnDesifrar.grid(row=2, column=0, padx=20, pady=10)
+
+        #desifrar_button = ctk.CTkButton(self, text="Ir a Desifrar", command=lambda: controller.show_page("DesifrarPage"))
+        #desifrar_button.grid(row=2, column=0, padx=20, pady=10)
--- v2/main.py
+++ v2/main.py
@@ -1,22 +1,34 @@
 import customtkinter as ctk
-from gui.inicio import InicioUi
-
-class MainWindow(ctk.CTk):
+from gui.inicio import InicioGui
+from gui.cifrar.cifrar import CifrarGui
+class App(ctk.CTk):
     def __init__(self):
         super().__init__()
-        self.title("Notas Finall")
+        self.title("Sha 256")
         self.geometry("600x400")
         self.resizable(False, False)
         
-        # Botón para abrir la ventana de inicio
-        open_button = ctk.CTkButton(self, text="Abrir Inicio", command=self.open_inicio)
-        open_button.pack(pady=20)
         
-    def open_inicio(self):
-        self.destroy()
-        inicio_ui = InicioUi()
-        inicio_ui.mainloop()
+        #frames contenedor para las paginas 
+        self.container=ctk.CTkFrame(self)
+        self.container.place(
+            relx=0, rely=0, relwidth=500, relheight=1
+        )
+        
+        self.paginas={}
+        self.crearPaginas()
+        self.mostrarPagina("Inicio")
 
+    def crearPaginas(self):
+        self.paginas["Inicio"] = InicioGui(parent=self.container, controller=self)
+        self.paginas["Cifrar"] = CifrarGui(parent=self.container, controller=self)
+        for pagina in self.paginas.values():
+            pagina.place(relx=0, rely=0, relwidth=1, relheight=1)
+    def mostrarPagina(self, pagina,titulo="Sha 256"):
+        self.title(titulo)
+        pagina=self.paginas[pagina]
+        pagina.tkraise()
+        
 if __name__ == "__main__":
-    app = MainWindow()
+    app = App()
     app.mainloop()
